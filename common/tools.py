818810b2b06d30cec2bd2378102114e801cf092d
HateBin <hatebin@163.com>
2025-02-21 16:47:23 +0800

    提交

--- common/tools.py
+++ common/tools.py
@@ -3,6 +3,9 @@ import settings
 import re
 import time
 import random
+import os
+import yaml
+import datetime
 
 
 def get_opts(name):
@@ -23,6 +26,7 @@ def text_mapping_switch(text):
         text = text.replace(key, value)
     return text
 
+
 def regular_expression(expression, text):
     """
     正则表达式
@@ -37,13 +41,103 @@ def regular_expression(expression, text):
         groups = result.groups()
     return groups
 
+
 def time_sleep(second=None):
     if not second:
         second = random.uniform(1, 3)
     time.sleep(second)
 
+def get_date(res_format: str = None, year=None, month=None, day=None, hour=None, minute=None, second=None):
+    """
+    获取日期和时间
+    :return:
+    """
+    now_date_time = datetime.datetime.now()
+    get_year = now_date_time.year + (year if year else 0)
+    get_month = now_date_time.month + (month if month else 0)
+    get_day = now_date_time.day + (day if day else 0)
+    get_hour = now_date_time.hour + (hour if hour else 0)
+    get_minute = now_date_time.minute + (minute if minute else 0)
+    get_second = now_date_time.second + (second if second else 0)
+    if res_format is None:
+        res_format = '%Y-%m-%d %H:%M:%S'
+    if '%Y' in res_format:
+        res_format = res_format.replace('%Y', '%04d' % get_year)
+    if '%m' in res_format:
+        res_format = res_format.replace('%m', '%02d' % get_month)
+    if '%d' in res_format:
+        res_format = res_format.replace('%d', '%02d' % get_day)
+    if '%H' in res_format:
+        res_format = res_format.replace('%H', '%02d' % get_hour)
+    if '%M' in res_format:
+        res_format = res_format.replace('%M', '%02d' % get_minute)
+    if '%S' in res_format:
+        res_format = res_format.replace('%S', '%02d' % get_second)
+    return res_format
+
+
+# 读取yaml文件
+def yaml_read(yaml_name, yaml_key=None, dic_name=None):
+    if dic_name:
+        path = os.path.dirname(os.path.dirname(__file__)) + f'\\{dic_name}\\{yaml_name}'
+    else:
+        path = os.path.dirname(os.path.dirname(__file__)) + f'\\yaml\\{yaml_name}'
+    is_exist = os.path.exists(path + '.yml')
+    if is_exist:
+        path += '.yml'
+    else:
+        is_exist = os.path.exists(path + '.yaml')
+        if is_exist:
+            path += '.yaml'
+    with open(path, 'r', encoding='utf-8') as f:
+        yaml_r = yaml.load(f, Loader=yaml.FullLoader)
+        if yaml_key:
+            return yaml_r.get(yaml_key)
+        else:
+            return yaml_r
+
+
+# 写入yaml文件
+def yaml_write(yaml_name, yaml_key, content, dic_name=None):
+    if dic_name:
+        path = os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + f'/{dic_name}/{yaml_name}'
+    else:
+        path = os.path.abspath(os.path.dirname(os.path.dirname(__file__))) + f'/yaml/{yaml_name}'
+    is_exist = os.path.exists(path + '.yml')
+    if is_exist:
+        path += '.yml'
+    else:
+        is_exist = os.path.exists(path + '.yaml')
+        if is_exist:
+            path += '.yaml'
+    with open(path, 'r', encoding='utf-8') as f:
+        yaml_r = yaml.safe_load(f)
+    if type(yaml_key) in (tuple, list):
+        if len(yaml_key) == 1:
+            yaml_r[yaml_key[0]] = content
+        if len(yaml_key) == 2:
+            yaml_r[yaml_key[0]][yaml_key[1]] = content
+    else:
+        yaml_r[yaml_key] = content
+    with open(path, 'w', encoding='utf-8') as f:
+        yaml.dump(yaml_r, f, default_flow_style=False, allow_unicode=True)
+    return 200
+
+def update_communicate_count(update: int = None, add: int = None):
+    current_date = get_date('%Y-%m-%d')
+    if update:
+        yaml_write('communicate_record', current_date, str(update))
+    if add:
+        current_count = yaml_read('communicate_record', current_date)
+        if not current_count:
+            current_count = 0
+            yaml_write('communicate_record', current_date, '0')
+        current_count = int(current_count) + add
+        yaml_write('communicate_record', current_date, str(current_count))
+
 
 if __name__ == '__main__':
-    exp = r'^(\d+)-(\d+)'
-    print(regular_expression(exp, '12-24K·13薪'))
-    print(regular_expression(exp, '11000-16000元/月'))
+    # exp = r'^(\d+)-(\d+)'
+    # print(regular_expression(exp, '12-24K·13薪'))
+    # print(regular_expression(exp, '11000-16000元/月'))
+    update_communicate_count(add=20)
--- page_locators/home_page_locators.py
+++ page_locators/home_page_locators.py
@@ -8,36 +8,55 @@
 class HomePageLocators:
     # 登录用户名定位
     user_name_locator = ('xpath', '/html/body/div[1]/div[1]/div/div/div[1]/div[3]/ul/li[2]/a/span')
-    # 职位选项定位
+    # 职位选项容器定位
     job_options_locator = ('xpath', '/html/body/div[1]/div[2]/div[1]/div/div[1]')
 
+    # 职位选项定位
     @staticmethod
     def job_option_locator(number):
         index = number + 1
         return 'xpath', f'/html/body/div[1]/div[2]/div[1]/div/div[1]/a[{index}]'
 
+    # 职位名称定位
     @staticmethod
     def job_name_locator(number):
         index = number + 1
         return 'xpath', f'/html/body/div[1]/div[2]/div[1]/div/div[1]/a[{index}]/span'
 
+    # 招聘薪资定位
     @staticmethod
     def hire_salary_locator(number):
         return 'xpath', f'/html/body/div[1]/div[2]/div[2]/div/div/div[1]/ul/div[{number}]/li/div[1]/div/span'
 
+    # 招聘标题定位
     @staticmethod
     def hire_title_locator(number):
         return 'xpath', f'/html/body/div[1]/div[2]/div[2]/div/div/div[1]/ul/div[{number}]/li/div[1]/div/a'
 
+    # 招聘选项定位
     @staticmethod
     def hire_option_locator(number):
         return 'xpath', f'/html/body/div[1]/div[2]/div[2]/div/div/div[1]/ul/div[{number}]/li'
 
+    # 招聘选项标签定位
+    @staticmethod
+    def hire_option_label_locator(number):
+        return 'xpath', f'/html/body/div[1]/div[2]/div[2]/div/div/div[1]/ul/div[{number}]/li/img'
+
+    # 招聘列表定位
     hire_list_locator = ('xpath', '/html/body/div[1]/div[2]/div[2]/div/div/div[1]/ul')
 
+    # 招聘详情描述定位
     hire_detail_msg_locator = ('xpath', '/html/body/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/p')
 
+    # 招聘者状态定位
+    recruiter_state_locator = ('xpath', '/html/body/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div[2]/h2/span')
+
+    # 沟通按钮定位
     communicate_button_locator = ('xpath', '/html/body/div[1]/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/a[2]')
 
+    # 沟通弹窗标题定位
     communicate_pop_title_locator = ('xpath', '/html/body/div[8]/div[2]/div[1]/h3')
+
+    # 沟通弹窗返回按钮定位
     communicate_pop_return_button_locator = ('xpath', '/html/body/div[8]/div[2]/div[3]/a[1]')
